190b061265083257c5cac156735deb5fdf887319
Song Joe <joe@Songs-MacBook-Pro.local>
2018-06-29 18:56:49 +0800

    Init

---
+++ app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const assert = require('assert');
+const Acm = require('./lib');
+
+// egg-acm/app.js
+module.exports = app => {
+  const config = app.config.acm;
+  assert(config.namespace, config.accessKey, config.secretKey, config.dataId, config.group);
+  const acm = Acm(config, app);
+  app.acm = acm;
+};
---
+++ config/config.default.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * egg-acm default config
+ * @member Config#acm
+ * @property {String} SOME_KEY - some description
+ */
+exports.acm = {
+  endpoint: 'acm.aliyun.com',
+  namespace: '',
+  accessKey: '',
+  secretKey: '',
+  requestTimeout: 6000,
+  dataId: '',
+  group: '',
+};
